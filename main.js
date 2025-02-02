96f2bc2919af51780514cdc0d232db043ac895f9
HadjerTalbi <hadjer.talbi2@etu.usthb.dz>
2025-02-02 22:52:30 +0100

    first commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function jump(){
 var checkDead = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 || characterTop>=130){
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
         alert("Nah Fashil");
