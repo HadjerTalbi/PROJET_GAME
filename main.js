8c6b727a8a66a5181f89f6a635beace8b1ca816c
HadjerTalbi <hadjer.talbi2@etu.usthb.dz>
2025-02-03 18:30:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,5 +15,30 @@ var checkDead = setInterval(function(){
         block.style.animation = "none";
         block.style.display = "none";
         alert("Nah Fashil");
+        showRestartButton();
     }
 },10);
+function showRestartButton() {
+    var restartButton = document.createElement("button");
+    restartButton.innerText = "Recommencer";
+    restartButton.style.position = "absolute";
+    restartButton.style.top = "50%";
+    restartButton.style.left = "50%";
+    restartButton.style.transform = "translate(-50%, -50%)";
+    document.body.appendChild(restartButton);
+
+   
+    restartButton.onclick = function() {
+        restartGame();
+        document.body.removeChild(restartButton); 
+    };
+}
+
+function restartGame() {
+    gameOver = false;
+    block.style.animation = "block 1s infinite linear"; 
+    block.style.display = "block"; 
+    block.style.left = "480px";
+
+    character.style.bottom = "0px";
+}
