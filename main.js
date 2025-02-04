68a6b585e3c445a6cfc29366c678c70aa0b1963d
HadjerTalbi <hadjer.talbi2@etu.usthb.dz>
2025-02-04 15:44:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ var checkDead = setInterval(function(){
     }
 },10);
 var Win = setInterval(function(){
+    if(blockLeft>=20 && blockLeft<=0 && characterTop<130){ 
         block.style.animation = "none";
         block.style.display = "none";
         alert("Great u won");
